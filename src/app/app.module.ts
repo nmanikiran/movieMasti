ec0c73e915911453e797bb52eb44666bc65c5804
nmanikiran <nmanikiran15@gmail.com>
2018-06-23 11:45:17 +0530

    updated ngsw path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -33,7 +33,7 @@ import { environment } from '../environments/environment';
     HttpClientModule,
     SharedModule,
     AppRoutingModule,
-    ServiceWorkerModule.register('./ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [MovieDBService],
   bootstrap: [AppComponent],
